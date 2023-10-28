44bd1ca53daf1cb6563297171f60c3356a2b97d9
Pedro Reis <pedroreisv@gmail.com>
2023-10-28 15:19:30 -0300

    feat: support more mouse events

--- src/app/HeatmapService.ts
+++ src/app/HeatmapService.ts
@@ -2,8 +2,23 @@ import HeatmapJS from "@/lib/heatmap";
 
 const MOVE_VAL = 1;
 const MIN_VAL = MOVE_VAL;
+const MOUSEUP_VAL = 2;
+const MOUSEDOWN_VAL = 3;
 const CLICK_VAL = 5;
-const MAX_VAL = 10;
+const MAX_VAL = 15;
+
+const mouseEventNameToVal = (name: string): number => {
+  switch (name) {
+    case "click":
+      return CLICK_VAL;
+    case "mousedown":
+      return MOUSEDOWN_VAL;
+    case "mouseup":
+      return MOUSEUP_VAL;
+    default:
+      return MOVE_VAL;
+  }
+};
 
 export class HeatmapService {
   constructor(private events: any[]) {}
@@ -23,16 +38,11 @@ export class HeatmapService {
       container: document.getElementById("heatmap") as HTMLElement,
     });
 
-    const data = this.mouseEvents.map((event: any) => {
-      const isClick = event.name === "click";
-      const { clientX, clientY } = event.properties;
-
-      return {
-        x: clientX,
-        y: clientY,
-        value: isClick ? CLICK_VAL : MOVE_VAL,
-      };
-    });
+    const data = this.mouseEvents.map((event) => ({
+      x: event.properties.clientX,
+      y: event.properties.clientY,
+      value: mouseEventNameToVal(event.name),
+    }));
 
     heatmap.setData({ min: MIN_VAL, max: MAX_VAL, data });
   }
