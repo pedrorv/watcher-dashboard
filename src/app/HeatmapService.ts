98dbd7136c5f7b99f9d87eb7d015a31b3e3d0c0b
Pedro Reis <pedroreisv@gmail.com>
2023-11-05 11:21:25 -0300

    feat: heatmap updates

--- src/app/HeatmapService.ts
+++ src/app/HeatmapService.ts
@@ -3,23 +3,35 @@ import HeatmapJS from "@/lib/heatmap";
 const MOVE_VAL = 1;
 const MIN_VAL = MOVE_VAL;
 const DRAG_VAL = MOVE_VAL;
+const WHEEL_VAL = 1.25;
+const DRAGENTER_VAL = MOVE_VAL;
+const DRAGLEAVE_VAL = MOVE_VAL;
+const DRAGEND_VAL = MOVE_VAL;
+const DRAGOVER_VAL = MOVE_VAL;
 const MOUSEUP_VAL = 2;
 const DRAGSTART_VAL = 3;
 const MOUSEDOWN_VAL = 3;
 const DROP_VAL = 5;
 const CLICK_VAL = 5;
+const DBCLICK_VAL = CLICK_VAL;
 const MAX_VAL = 15;
 
 const mouseEventNameToVal = (name: string): number => {
   switch (name) {
     case "click":
       return CLICK_VAL;
+    case "dbclick":
+      return DBCLICK_VAL;
     case "mousedown":
       return MOUSEDOWN_VAL;
     case "mouseup":
       return MOUSEUP_VAL;
-    default:
+    case "mousemove":
       return MOVE_VAL;
+    case "wheel":
+      return WHEEL_VAL;
+    default:
+      return 0;
   }
 };
 
@@ -31,6 +43,14 @@ const dragEventNameToVal = (name: string): number => {
       return DRAGSTART_VAL;
     case "drop":
       return DROP_VAL;
+    case "dragenter":
+      return DRAGENTER_VAL;
+    case "dragleave":
+      return DRAGLEAVE_VAL;
+    case "dragover":
+      return DRAGOVER_VAL;
+    case "dragend":
+      return DRAGEND_VAL;
     default:
       return 0;
   }
@@ -68,7 +88,9 @@ export class HeatmapService {
       value: mouseEventNameToVal(event.name),
     }));
 
-    const data = mouseEventsAsData.concat(dragEventsAsData);
+    const data = mouseEventsAsData
+      .concat(dragEventsAsData)
+      .filter((data) => data.value);
 
     heatmap.setData({ min: MIN_VAL, max: MAX_VAL, data });
   }
