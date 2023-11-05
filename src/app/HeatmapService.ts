4d8e77b5218f87d7ecfc6e7eec35faff89e99b47
Pedro Reis <pedroreisv@gmail.com>
2023-11-05 11:04:45 -0300

    feat: heatmap displaying drag events

--- src/app/HeatmapService.ts
+++ src/app/HeatmapService.ts
@@ -2,8 +2,11 @@ import HeatmapJS from "@/lib/heatmap";
 
 const MOVE_VAL = 1;
 const MIN_VAL = MOVE_VAL;
+const DRAG_VAL = MOVE_VAL;
 const MOUSEUP_VAL = 2;
+const DRAGSTART_VAL = 3;
 const MOUSEDOWN_VAL = 3;
+const DROP_VAL = 5;
 const CLICK_VAL = 5;
 const MAX_VAL = 15;
 
@@ -20,11 +23,28 @@ const mouseEventNameToVal = (name: string): number => {
   }
 };
 
+const dragEventNameToVal = (name: string): number => {
+  switch (name) {
+    case "drag":
+      return DRAG_VAL;
+    case "dragstart":
+      return DRAGSTART_VAL;
+    case "drop":
+      return DROP_VAL;
+    default:
+      return 0;
+  }
+};
+
 export class HeatmapService {
   constructor(private events: any[]) {}
 
   static filterHeatmapEvents(events: any[]) {
-    return events.filter((e) => ["ui", "mouse"].includes(e.type));
+    return events.filter((e) => ["ui", "mouse", "drag"].includes(e.type));
+  }
+
+  private get dragEvents(): any[] {
+    return this.events.filter((e) => e.type === "drag");
   }
 
   private get mouseEvents(): any[] {
@@ -36,11 +56,19 @@ export class HeatmapService {
       container: document.getElementById("heatmap") as HTMLElement,
     });
 
-    const data = this.mouseEvents.map((event) => ({
+    const dragEventsAsData = this.dragEvents.map((event) => ({
+      x: event.properties.pageX ?? event.properties.clientX,
+      y: event.properties.pageY ?? event.properties.clientY,
+      value: dragEventNameToVal(event.name),
+    }));
+
+    const mouseEventsAsData = this.mouseEvents.map((event) => ({
       x: event.properties.pageX ?? event.properties.clientX,
       y: event.properties.pageY ?? event.properties.clientY,
       value: mouseEventNameToVal(event.name),
     }));
+
+    const data = mouseEventsAsData.concat(dragEventsAsData);
 
     heatmap.setData({ min: MIN_VAL, max: MAX_VAL, data });
   }
